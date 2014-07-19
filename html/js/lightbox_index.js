d041c2199b620a919e22d484fbb9496932474a3f
Arjuna <arjun.kashyap@yahoo.co.in>
2014-07-19 21:34:23 +0530

    Commit No. 6 - Bug fixes in pl and xml; Final version

--- html/js/lightbox_index.js
+++ html/js/lightbox_index.js
@@ -46,8 +46,8 @@
 //  Configurationl
 //
 LightboxOptions = Object.extend({
-    fileLoadingImage:        'php/images/loading.gif',     
-    fileBottomNavCloseImage: 'php/images/closelabel.gif',
+    fileLoadingImage:        'html/images/loading.gif',     
+    fileBottomNavCloseImage: 'html/images/closelabel.gif',
 
     overlayOpacity: 0.8,   // controls transparency of shadow overlay
 
